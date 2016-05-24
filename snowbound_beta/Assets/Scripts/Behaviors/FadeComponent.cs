28c8b791d7d620731cd757a9f2988ce5751fde3f
aking <psy_wombats@wombatrpgs.net>
2016-05-23 23:44:16 -0700

    Half of a functioning title system?

---
+++ snowbound_beta/Assets/Scripts/Behaviors/FadeComponent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeComponent : MonoBehaviour {
+
+    public Image image = null;
+    public bool autoFadeIn = false;
+    public float fadeTime = 0.8f;
+
+    private float Alpha {
+        get { return image.GetComponent<CanvasRenderer>().GetColor().a; }
+        set { image.GetComponent<CanvasRenderer>().SetAlpha(value); }
+    }
+
+    public void Awake() {
+        if (autoFadeIn) {
+            Alpha = 1.0f;
+        }
+    }
+
+    public void Start() {
+        if (autoFadeIn) {
+            FadeIn();
+            autoFadeIn = false;
+        }
+    }
+
+    public void FadeIn() {
+        StartCoroutine(FadeInRoutine());
+    }
+
+    public void FadeOut() {
+        StartCoroutine(FadeOutRoutine());
+    }
+    
+    public IEnumerator FadeInRoutine() {
+        while (Alpha > 0.0f) {
+            Alpha -= Time.deltaTime / fadeTime;
+            yield return null;
+        }
+        if (Alpha < 0.0f) {
+            Alpha = 0.0f;
+        }
+    }
+
+    public IEnumerator FadeOutRoutine() {
+        while (Alpha < 1.0f) {
+            Alpha += Time.deltaTime / fadeTime;
+            yield return null;
+        }
+        if (Alpha > 1.0f) {
+            Alpha = 1.0f;
+        }
+    }
+}
---
+++ snowbound_beta/Assets/Scripts/Behaviors/MainMenuComponent.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuComponent : MonoBehaviour {
+
+    public Button startButton;
+    public Button loadButton;
+    public Button quitButton;
+
+    private FadeComponent fade;
+
+    public void Awake() {
+        fade = FindObjectOfType<FadeComponent>();
+
+        startButton.onClick.AddListener(() => {
+
+        });
+        loadButton.onClick.AddListener(() => {
+
+        });
+        quitButton.onClick.AddListener(() => {
+
+        });
+    }
+
+    private IEnumerator StartRoutine() {
+        yield return null;
+    }
+
+    private IEnumerator LoadRoutine() {
+        GameObject loadMenuObject = SaveMenuComponent.Spawn(null, SaveMenuComponent.SaveMenuMode.Load);
+        loadMenuObject.GetComponent<SaveMenuComponent>().Alpha = 0.0f;
+        yield return StartCoroutine(loadMenuObject.GetComponent<SaveMenuComponent>().FadeIn());
+    }
+
+    private IEnumerator QuitRoutine() {
+        yield return fade.FadeOutRoutine();
+        Application.Quit();
+    }
+}
