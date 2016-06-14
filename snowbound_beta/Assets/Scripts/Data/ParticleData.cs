e29c569b89259e8d78997de250a1788976429030
aking <psy_wombats@wombatrpgs.net>
2016-06-13 21:18:21 -0700

    The particle effect ain't gonna work.

---
+++ snowbound_beta/Assets/Scripts/Data/ParticleData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ParticleData", menuName = "Data/ParticleData")]
+public class ParticleData : GenericDataObject {
+
+    public Particle backgroundParticle;
+    public Particle foregroundParticle;
+
+}
