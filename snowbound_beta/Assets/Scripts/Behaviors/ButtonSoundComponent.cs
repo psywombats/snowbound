dd0425bdca9baceae8a23d4023d22fc712784eb3
aking <psy_wombats@wombatrpgs.net>
2016-06-14 21:10:18 -0700

    Basic scripting for button sfx

---
+++ snowbound_beta/Assets/Scripts/Behaviors/ButtonSoundComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Button))]
+public class ButtonSoundComponent : MonoBehaviour {
+
+    private const string DefaultHoverSoundTag = null;
+    private const string DefaultClickSoundTag = "click";
+
+    public string ClickSoundTag = "";
+    public string HoverSoundTag = "";
+
+    private SoundPlayer player;
+
+    public void Awake() {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() => {
+            HandleButtonEvent(ClickSoundTag.Length > 0 ? ClickSoundTag : DefaultClickSoundTag);
+        });
+        button.onClick.AddListener(() => {
+            HandleButtonEvent(ClickSoundTag.Length > 0 ? ClickSoundTag : DefaultClickSoundTag);
+        });
+
+        player = FindObjectOfType<SoundPlayer>();
+    }
+
+    private void HandleButtonEvent(string associatedTag) {
+        player.PlaySound(associatedTag);
+    }
+}
--- snowbound_beta/Assets/Scripts/Data/ParticleData.cs
+++ snowbound_beta/Assets/Scripts/Data/ParticleData.cs
@@ -7,3 +7,4 @@ public class ParticleData : GenericDataObject {
     public Particle foregroundParticle;
 
 }
+
